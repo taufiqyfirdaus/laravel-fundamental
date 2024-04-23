91f226cd6f8e23b8f57834c98fae229fa7168880
Taufiqy <taufiqyfirdaus@gmail.com>
2024-04-23 19:25:01 +0700

    Update route web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/list-menu/get-all-data', function () {
+    $data = [
+        ['makanan' => 'Soto Mie', 'harga' => 8000],
+        ['makanan' => 'Bakso Malang', 'harga' => 13000],
+        ['makanan' => 'Ketoprak', 'harga' => 15000]
+    ];
+
+    return response()->json([
+        'info' => 'Data berhasil didapatkan',
+        'data' => $data
+    ]);
+});
+
+Route::get('/list-menu', function () {
+    return 'Ini adalah halaman yang menampilkan semua menu yang ada di Cafe Amandemy';
+});
+
+Route::get('/list-menu/{makanan}/{harga}', function ($makanan, $harga) {
+    return "Anda memilih makanan $makanan dengan harga $harga";
 });
